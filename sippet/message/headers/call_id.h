88dc163ff36a6db29b0c848c79d8f345e1f1178e
Guilherme B. Versiani <guibv@yahoo.com>
2013-06-03 15:26:28 +0000

    - More headers.

---
+++ sippet/message/headers/call_id.h
@@ -0,0 +1,58 @@
+/* 
+ * Copyright (c) 2013, Guilherme Balena Versiani
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies, 
+ * either expressed or implied, of the FreeBSD Project.
+ */
+
+#ifndef SIPPET_MESSAGE_HEADERS_CALL_ID_H_
+#define SIPPET_MESSAGE_HEADERS_CALL_ID_H_
+
+#include <string>
+#include "sippet/message/header.h"
+#include "sippet/base/raw_ostream.h"
+
+namespace sippet {
+
+class CallId :
+  public Header {
+public:
+  CallId() : Header(Header::HDR_CALL_ID) {}
+
+  void set_value(const std::string &value) { value_ = value; }
+  std::string value() const { return value_; }
+
+  virtual void print(raw_ostream &os) const {
+    os.write_hname("Call-ID");
+    os << value();
+  }
+
+private:
+  std::string value_;
+};
+
+} // End of sippet namespace
+
+#endif // SIPPET_MESSAGE_HEADERS_CALL_ID_H_
