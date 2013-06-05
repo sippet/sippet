6488817f20dec36c9d587a69922275dd7ea40f1d
david.sippet@gmail.com <david.sippet@gmail.com@c52d63f2-dbfe-55b4-363d-0e9f0fd5b8f5>
2013-06-05 17:50:58 +0000

    - Included more headers.

---
+++ sippet/message/headers/server.h
@@ -0,0 +1,66 @@
+/* 
+ * Copyright (c) 2013, Guilherme Balena Versiani
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies, 
+ * either expressed or implied, of the FreeBSD Project.
+ */
+
+#ifndef SIPPET_MESSAGE_HEADERS_SERVER_H_
+#define SIPPET_MESSAGE_HEADERS_SERVER_H_
+
+#include <string>
+#include "sippet/message/header.h"
+#include "sippet/message/headers/bits/single_value.h"
+#include "sippet/base/raw_ostream.h"
+
+namespace sippet {
+
+class Server :
+  public Header,
+  public single_value<std::string> {
+private:
+  Server(const Server &other) : Header(other), single_value(other) {}
+  Server &operator=(const Server &other);
+  virtual Server *DoClone() const {
+    return new Server(*this);
+  }
+public:
+  Server() : Header(Header::HDR_SERVER) {}
+  Server(const single_value::value_type &subject)
+    : Header(Header::HDR_SERVER), single_value(subject) {}
+
+  scoped_ptr<Server> Clone() const {
+    return scoped_ptr<Server>(DoClone());
+  }
+
+  virtual void print(raw_ostream &os) const {
+    os.write_hname("User-Agent");
+    single_value::print(os);
+  }
+};
+
+} // End of sippet namespace
+
+#endif // SIPPET_MESSAGE_HEADERS_SERVER_H_
