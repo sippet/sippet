493055a36bface6b57685f00f89804f6906a75a5
david.sippet@gmail.com <david.sippet@gmail.com@c52d63f2-dbfe-55b4-363d-0e9f0fd5b8f5>
2013-06-05 18:56:58 +0000

    - More headers.

---
+++ trunk/sippet/message/headers/to.h
@@ -0,0 +1,64 @@
+/* 
+ * Copyright (c) 2013, Guilherme Balena Versiani
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met: 
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer. 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are those
+ * of the authors and should not be interpreted as representing official policies, 
+ * either expressed or implied, of the FreeBSD Project.
+ */
+
+#ifndef SIPPET_MESSAGE_HEADERS_TO_H_
+#define SIPPET_MESSAGE_HEADERS_TO_H_
+
+#include "sippet/message/headers/contact.h"
+
+namespace sippet {
+
+class To :
+  public Header,
+  public contact_info {
+private:
+  To(const To &other)
+    : Header(other), contact_info(other) {}
+  To &operator=(const To &);
+  virtual To *DoClone() const {
+    return new To(*this);
+  }
+public:
+  To() : Header(Header::HDR_TO) {}
+  To(const std::string &address, const std::string &displayName="")
+    : Header(Header::HDR_TO), contact_info(address, displayName) {}
+
+  scoped_ptr<To> Clone() const {
+    return scoped_ptr<To>(DoClone());
+  }
+
+  virtual void print(raw_ostream &os) const {
+    os.write_hname("To");
+    contact_info::print(os);
+  }
+};
+
+} // End of sippet namespace
+
+#endif // SIPPET_MESSAGE_HEADERS_TO_H_
