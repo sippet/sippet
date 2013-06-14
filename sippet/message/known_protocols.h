a700fb7054e24903f476c7ede144ceecac6172e3
Guilherme B. Versiani <guibv@yahoo.com>
2013-06-14 16:05:12 +0000

    - Changed the licenses.

--- sippet/message/known_protocols.h
+++ sippet/message/known_protocols.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2013 The Sippet Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 // All supported protocols must be kept in lexicographical order
 // of their header names.
 //
