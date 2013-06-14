6a7a6a41ee17aa45989fdde1275cbb01132c9a1e
david.sippet@gmail.com <david.sippet@gmail.com@c52d63f2-dbfe-55b4-363d-0e9f0fd5b8f5>
2013-06-14 16:05:12 +0000

    - Changed the licenses.

--- trunk/sippet/message/headers/unsupported.h
+++ trunk/sippet/message/headers/unsupported.h
@@ -1,3 +1,6 @@
+// Copyright (c) 2013 The Sippet Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 /*
  * Copyright (c) 2013, Guilherme Balena Versiani
  * All rights reserved.
