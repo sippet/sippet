6815ada0194b0dc39035a556bbe5257a2edf4df0
david.sippet@gmail.com <david.sippet@gmail.com@c52d63f2-dbfe-55b4-363d-0e9f0fd5b8f5>
2014-07-04 15:13:14 +0000

    - Including User-Agent and Server headers to requests and responses, respectively, when sent by the NetworkLayer.

---
+++ sippet/base/user_agent_utils_ios.mm
@@ -0,0 +1,95 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "sippet/base/user_agent_util.h"
+
+#import <UIKit/UIKit.h>
+
+#include <sys/sysctl.h>
+#include <string>
+
+#include "base/mac/scoped_nsobject.h"
+#include "base/strings/string_util.h"
+#include "base/strings/stringprintf.h"
+#include "base/strings/sys_string_conversions.h"
+#include "base/sys_info.h"
+
+namespace sippet {
+
+std::string BuildOSCpuInfo() {
+  int32 os_major_version = 0;
+  int32 os_minor_version = 0;
+  int32 os_bugfix_version = 0;
+  base::SysInfo::OperatingSystemVersionNumbers(&os_major_version,
+                                               &os_minor_version,
+                                               &os_bugfix_version);
+  std::string os_version;
+  if (os_bugfix_version == 0) {
+    base::StringAppendF(&os_version,
+                        "%d_%d",
+                        os_major_version,
+                        os_minor_version);
+  } else {
+    base::StringAppendF(&os_version,
+                        "%d_%d_%d",
+                        os_major_version,
+                        os_minor_version,
+                        os_bugfix_version);
+  }
+
+  // Remove the end of the platform name. For example "iPod touch" becomes
+  // "iPod".
+  std::string platform = base::SysNSStringToUTF8(
+      [[UIDevice currentDevice] model]);
+  size_t position = platform.find_first_of(" ");
+  if (position != std::string::npos)
+    platform = platform.substr(0, position);
+
+  std::string os_cpu;
+  base::StringAppendF(
+      &os_cpu,
+      "%s; CPU %s %s like Mac OS X",
+      platform.c_str(),
+      (platform == "iPad") ? "OS" : "iPhone OS",
+      os_version.c_str());
+
+  return os_cpu;
+}
+
+std::string BuildUserAgentFromProduct(const std::string& product) {
+  // Retrieve the kernel build number.
+  int mib[2] = {CTL_KERN, KERN_OSVERSION};
+  unsigned int namelen = sizeof(mib) / sizeof(mib[0]);
+  size_t bufferSize = 0;
+  sysctl(mib, namelen, NULL, &bufferSize, NULL, 0);
+  char kernel_version[bufferSize];
+  int result = sysctl(mib, namelen, kernel_version, &bufferSize, NULL, 0);
+  DCHECK(result == 0);
+
+  std::string user_agent;
+  base::StringAppendF(
+      &user_agent,
+      "Sippet/%d.%d (%s)",
+      GetSippetMajorVersion(),
+      GetSippetMinorVersion(),
+      BuildOSCpuInfo().c_str());
+
+  if (!product.empty()) {
+    base::StringAppendF(
+        &user_agent,
+        " %s",
+        product.c_str()
+		);
+  }
+
+  base::StringAppendF(
+      &user_agent,
+      " Mobile/%s",
+      kernel_version
+	  );
+
+  return user_agent;
+}
+
+}  // namespace webkit_glue
