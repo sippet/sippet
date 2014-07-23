56a051b7c13c7f76198729fa7b912cf110395b6f
david.sippet@gmail.com <david.sippet@gmail.com@c52d63f2-dbfe-55b4-363d-0e9f0fd5b8f5>
2014-07-23 21:12:31 +0000

    - Added another test case for AuthController (NoExplicitCredentialsAllowed).

--- trunk/sippet/ua/auth_controller_unittest.cc
+++ trunk/sippet/ua/auth_controller_unittest.cc
@@ -6,12 +6,14 @@
 #include "sippet/ua/auth_controller.h"
 #include "sippet/ua/auth_cache.h"
 #include "base/message_loop/message_loop.h"
+#include "base/strings/utf_string_conversions.h"
 #include "net/base/net_errors.h"
 #include "net/base/test_completion_callback.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using ::testing::NiceMock;
 using ::testing::Return;
+using ::testing::Invoke;
 using ::testing::_;
 
 namespace sippet {
@@ -66,11 +68,11 @@ class AuthControllerTest : public testing::Test {
 
     current_test_ = test;
     scoped_refptr<Request> request(dyn_cast<Request>(
-      Request::Parse("REGISTER sip:some.domain.com SIP/2.0\r\n"
+      Message::Parse("REGISTER sip:some.domain.com SIP/2.0\r\n"
                      "\r\n")));
     request->set_direction(Message::Incoming);
     scoped_refptr<Response> response(dyn_cast<Response>(
-      Response::Parse(test->response_)));
+      Message::Parse(test->response_)));
     response->set_refer_to(request);
 
     NiceMock<AuthHandlerMock>* auth_handler = new NiceMock<AuthHandlerMock>;
@@ -224,6 +226,7 @@ TEST_F(AuthControllerTest, UnexpectedSecurityLibraryStatus) {
     net::ERR_UNEXPECTED_SECURITY_LIBRARY_STATUS,
     net::OK,
     net::OK };
+
   RunResponseTest(&case1);
 }
 
@@ -240,6 +243,7 @@ TEST_F(AuthControllerTest, MissingAuthCredentials) {
     net::ERR_MISSING_AUTH_CREDENTIALS,
     net::OK,
     net::OK };
+
   RunResponseTest(&case2);
 }
 
@@ -254,11 +258,122 @@ TEST_F(AuthControllerTest, InvalidAuthCredentials) {
     net::ERR_INVALID_AUTH_CREDENTIALS,
     net::OK,
     net::ERR_INVALID_AUTH_CREDENTIALS };
+
   RunResponseTest(&case3);
 }
 
+// If an AuthHandler indicates that it doesn't allow explicit
+// credentials, don't prompt for credentials.
 TEST_F(AuthControllerTest, NoExplicitCredentialsAllowed) {
-  // TODO
+  // Modified mock HttpAuthHandler for this test.
+  class MockHandler : public AuthHandlerMock {
+   public:
+    MockHandler(int expected_rv, Auth::Scheme scheme)
+        : expected_scheme_(scheme) {
+      ON_CALL(*this, GenerateAuthImpl(_, _, _))
+        .WillByDefault(Return(expected_rv));
+      ON_CALL(*this, HandleAnotherChallenge(_))
+        .WillByDefault(Invoke(this, &MockHandler::HandleAnotherChallenge));
+    }
+
+   protected:
+    virtual bool Init(const Challenge& challenge) OVERRIDE {
+      AuthHandlerMock::Init(challenge);
+      set_allows_default_credentials(true);
+      set_allows_explicit_credentials(false);
+      // Pretend to be SCHEME_DIGEST so we can test failover logic.
+      if (challenge.scheme() == "Digest") {
+        auth_scheme_ = net::HttpAuth::AUTH_SCHEME_DIGEST;
+        --score_;  // Reduce score, so we rank below Mock.
+        set_allows_explicit_credentials(true);
+      }
+      EXPECT_EQ(expected_scheme_, auth_scheme_);
+      return true;
+    }
+
+    Auth::AuthorizationResult HandleAnotherChallenge(
+        const Challenge& challenge) {
+      // If we receive an empty challenge for a connection based scheme, or a second
+      // challenge for a non connection based scheme, assume it's a rejection.
+      if (challenge.param_empty())
+        return net::HttpAuth::AUTHORIZATION_RESULT_REJECT;
+      if (!LowerCaseEqualsASCII(challenge.scheme(), "mock"))
+        return net::HttpAuth::AUTHORIZATION_RESULT_INVALID;
+      return net::HttpAuth::AUTHORIZATION_RESULT_ACCEPT;
+    }
+
+   private:
+    Auth::Scheme expected_scheme_;
+  };
+
+  net::BoundNetLog dummy_log;
+  AuthCache dummy_auth_cache;
+  scoped_refptr<Request> request(dyn_cast<Request>(
+    Message::Parse("REGISTER sip:some.domain.com SIP/2.0\r\n"
+                   "\r\n")));
+  request->set_direction(Message::Incoming);
+  scoped_refptr<Response> response(dyn_cast<Response>(
+      Message::Parse("SIP/2.0 401\r\n"
+                     "WWW-Authenticate: Mock\r\n"
+                     "WWW-Authenticate: Digest\r\n"
+                     "\r\n")));
+  response->set_refer_to(request);
+
+  // Handlers for the first attempt at authentication.  AUTH_SCHEME_MOCK handler
+  // accepts the default identity and successfully constructs a token.
+  factory().AddMockHandler(
+      new NiceMock<MockHandler>(net::OK, net::HttpAuth::AUTH_SCHEME_MOCK),
+      net::HttpAuth::AUTH_SERVER, true);
+  factory().AddMockHandler(
+      new NiceMock<MockHandler>(net::ERR_UNEXPECTED, net::HttpAuth::AUTH_SCHEME_DIGEST),
+      net::HttpAuth::AUTH_SERVER, true);
+
+  // Handlers for the second attempt.  Neither should be used to generate a
+  // token.  Instead the controller should realize that there are no viable
+  // identities to use with the AUTH_SCHEME_MOCK handler and fail.
+  factory().AddMockHandler(
+      new NiceMock<MockHandler>(net::ERR_UNEXPECTED, net::HttpAuth::AUTH_SCHEME_MOCK),
+      net::HttpAuth::AUTH_SERVER, true);
+  factory().AddMockHandler(
+      new NiceMock<MockHandler>(net::ERR_UNEXPECTED, net::HttpAuth::AUTH_SCHEME_DIGEST),
+      net::HttpAuth::AUTH_SERVER, true);
+
+  // Fallback handlers for the second attempt.  The AUTH_SCHEME_MOCK handler
+  // should be discarded due to the disabled scheme, and the AUTH_SCHEME_BASIC
+  // handler should successfully be used to generate a token.
+  factory().AddMockHandler(
+      new NiceMock<MockHandler>(net::ERR_UNEXPECTED, net::HttpAuth::AUTH_SCHEME_MOCK),
+      net::HttpAuth::AUTH_SERVER, true);
+  factory().AddMockHandler(
+      new NiceMock<MockHandler>(net::OK, net::HttpAuth::AUTH_SCHEME_DIGEST),
+      net::HttpAuth::AUTH_SERVER, true);
+
+  ASSERT_EQ(net::OK,
+            controller().HandleAuthChallenge(response, dummy_log));
+  ASSERT_TRUE(controller().HaveAuthHandler());
+  controller().ResetAuth(net::AuthCredentials());
+  EXPECT_TRUE(controller().HaveAuth());
+
+  // Should only succeed if we are using the AUTH_SCHEME_MOCK MockHandler.
+  EXPECT_EQ(net::OK, controller().AddAuthorizationHeaders(
+      request, net::CompletionCallback(), dummy_log));
+
+  // Once a token is generated, simulate the receipt of a server response
+  // indicating that the authentication attempt was rejected.
+  ASSERT_EQ(net::OK,
+            controller().HandleAuthChallenge(response, dummy_log));
+  ASSERT_TRUE(controller().HaveAuthHandler());
+  controller().ResetAuth(net::AuthCredentials(ASCIIToUTF16("Hello"),
+                         base::string16()));
+  EXPECT_TRUE(controller().HaveAuth());
+  EXPECT_TRUE(controller().IsAuthSchemeDisabled(
+    net::HttpAuth::AUTH_SCHEME_MOCK));
+  EXPECT_FALSE(controller().IsAuthSchemeDisabled(
+    net::HttpAuth::AUTH_SCHEME_DIGEST));
+
+  // Should only succeed if we are using the AUTH_SCHEME_BASIC MockHandler.
+  EXPECT_EQ(net::OK, controller().AddAuthorizationHeaders(
+      request, net::CompletionCallback(), dummy_log));
 }
 
 } // namespace sippet
