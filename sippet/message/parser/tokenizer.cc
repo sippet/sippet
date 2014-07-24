b774570d43549beeb516e5f5d8c0b16ec1327fec
Guilherme B. Versiani <guibv@yahoo.com>
2014-07-24 19:40:01 +0000

    - Adjusting the code to match Chromium style.

---
+++ sippet/message/parser/tokenizer.cc
@@ -0,0 +1,17 @@
+// Copyright (c) 2013 The Sippet Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "sippet/message/parser/tokenizer.h"
+
+namespace sippet {
+
+Tokenizer::Tokenizer(std::string::const_iterator string_begin,
+                     std::string::const_iterator string_end)
+  : current_(string_begin), end_(string_end) {
+}
+
+Tokenizer::~Tokenizer() {
+}
+
+} // namespace sippet
