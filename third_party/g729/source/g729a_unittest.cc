6fce0b61d511169321e661575ae6b7d044937626
Guilherme B. Versiani <guibv@yahoo.com>
2015-03-02 16:31:21 +0000

    Added more unit tests.

--- third_party/g729/source/g729a_unittest.cc
+++ third_party/g729/source/g729a_unittest.cc
@@ -70,31 +70,40 @@ class G729Test : public testing::Test {
   void SetUp() override {
   }
  protected:
-  static const UWord8 expected_bits_[10];
-  static const Word16 expected_parm_[PRM_SIZE];
+  static const UWord8 expected_bits_[2][10];
+  static const Word16 expected_parm_[2][PRM_SIZE];
 };
 
-const UWord8 G729Test::expected_bits_[10] = {
-  0x1a, 0xc1, 0x00, 0xa0, 0x00, 0xfa, 0xc2, 0xff, 0xfd, 0x50,
+const UWord8 G729Test::expected_bits_[2][10] = {
+  { 0x1a, 0xc1, 0x00, 0xa0, 0x00, 0xfa, 0xc2, 0xff, 0xfd, 0x50 },
+  { 0xe0, 0xA2, 0xD4, 0x21, 0x41, 0x69, 0x59, 0xEF, 0x20, 0x50 },
 };
 
-const Word16 G729Test::expected_parm_[PRM_SIZE] = {
-  0x001a, 0x0304, 0x0002, 0x0001, 0x0000, 0x000f,
-  0x0056, 0x0002, 0x1fff, 0x000a, 0x0050,
+const Word16 G729Test::expected_parm_[2][PRM_SIZE] = {
+  { 0x001a, 0x0304, 0x0002, 0x0001, 0x0000, 0x000f,
+    0x0056, 0x0002, 0x1fff, 0x000a, 0x0050 },
+  { 0x00e0, 0x028b, 0x0050, 0x0001, 0x0141, 0x0006,
+    0x004a, 0x0019, 0x1de4, 0x0000, 0x0050 },
 };
 
 TEST_F(G729Test, bits2prm) {
   Word16 output_parm[PRM_SIZE];
-  bits2prm_ld8k(expected_bits_, output_parm);
 
-  EXPECT_PRED_FORMAT3(AssertSameArrays, expected_parm_, output_parm,
-    PRM_SIZE);
+  for (int i = 0; i < sizeof(expected_bits_)/sizeof(expected_bits_[0]); i++) {
+    bits2prm_ld8k(expected_bits_[i], output_parm);
+
+    EXPECT_PRED_FORMAT3(AssertSameArrays,
+      expected_parm_[i], output_parm, PRM_SIZE);
+  }
 }
 
 TEST_F(G729Test, prm2bits) {
   UWord8 output_bits[10];
-  prm2bits_ld8k(expected_parm_, output_bits);
+  
+  for (int i = 0; i < sizeof(expected_bits_) / sizeof(expected_bits_[0]); i++) {
+    prm2bits_ld8k(expected_parm_[i], output_bits);
 
-  EXPECT_PRED_FORMAT3(AssertSameArrays, expected_bits_, output_bits,
-    10);
+    EXPECT_PRED_FORMAT3(AssertSameArrays, expected_bits_[i], output_bits,
+      10);
+  }
 }
